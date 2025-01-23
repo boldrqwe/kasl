1b09ec46d86512f48b425502f6766a6d4a364722
lalala <lbondyaaa@gmail.com>
2025-01-23 23:11:04 +0300

    init6

--- src/run_container.py
+++ src/run_container.py
@@ -1,34 +1,40 @@
-import subprocess
+import docker
 
 def run_docker_container():
-    # Настройки контейнера
+    # Инициализация Docker клиента
+    client = docker.from_env()
+
+    # Параметры контейнера
     image = "jlesage/firefox"
     container_name = "firefox_container"
     vnc_password = "123"
     ports = {
-        "5800": "5800",
-        "5900": "5900"
+        '5800/tcp': 5800,
+        '5900/tcp': 5900,
     }
 
-    # Формируем команду docker run
-    docker_run_command = [
-        "docker", "run", "-d",
-        "--name", container_name,
-        "-p", f"{ports['5800']}:5800",
-        "-p", f"{ports['5900']}:5900",
-        "--env", f"VNC_PASSWORD={vnc_password}",
-        image
-    ]
-
     try:
-        # Запуск команды
-        result = subprocess.run(docker_run_command, capture_output=True, text=True)
-        if result.returncode == 0:
-            print(f"Контейнер успешно запущен! ID контейнера: {result.stdout.strip()}")
-        else:
-            print(f"Ошибка запуска контейнера: {result.stderr.strip()}")
+        # Проверка на существующий контейнер
+        existing_container = client.containers.list(
+            all=True, filters={"name": container_name}
+        )
+        if existing_container:
+            print(f"Контейнер с именем '{container_name}' уже существует. Удаляю...")
+            existing_container[0].remove(force=True)
+
+        # Запуск нового контейнера
+        print("Запускаем контейнер...")
+        container = client.containers.run(
+            image,
+            detach=True,
+            name=container_name,
+            ports=ports,
+            environment={"VNC_PASSWORD": vnc_password},
+        )
+        print(f"Контейнер успешно запущен! ID контейнера: {container.id}")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
+
 
 if __name__ == "__main__":
     run_docker_container()
